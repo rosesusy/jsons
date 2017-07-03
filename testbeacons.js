691f513c3827a7fd743a6197afee57f346814326
rosesusy <f.rosesusy@gmail.com>
2017-07-03 13:55:02 +0200

    Create testbeacons.js

---
+++ testbeacons.js
@@ -0,0 +1,33 @@
+[
+{
+  "major":"1",
+  "minor":"1",
+  "idName":"1,1",
+  "triggerDistance":"0.68",
+  "pois":[
+          {
+            "name":"Retyo1", 
+            "distance":"2.00",
+            "direction":"120",
+            "kind":"WC",
+            "comment":"Jo szarast1"
+          },
+          {
+            "name":"Terem1",
+            "distance":"1.00",
+            "direction":"130",
+            "kind":"Terem",
+            "comment":"Jo tanulast1"
+          }
+         ],
+    "neighbourBeacons":
+      {
+      "1,2":
+            { 
+              "distance":"8.0",
+              "direction":"123.23",
+              "comment":"Comment a 1,2es beaconhoz vezeto utra"
+              }
+        }
+ },
+ {"major":"1","minor":"2","idName":"1,2","triggerDistance":"0.69","pois":[{"name":"Retyo2","distance":"4.00","direction":"120","kind":"WC","comment":"Jo szarast2"},{"name":"Terem2","distance":"6.00","direction":"130","kind":"Terem","comment":"Jo tanulast2"}],"neighbourBeacons":{"1,1":{"distance":"8.00","direction":"123.23","comment":"Comment a 1,1es beaconhoz vezeto utra"},"1,3":{"distance":"15.00","direction":"123.23","comment":"Comment a 1,3es beaconhoz vezeto utra"}}},{"major":"1","minor":"3","idName":"1,3","triggerDistance":"0.70","pois":[{"name":"Retyo3","distance":"3.00","direction":"120","kind":"WC","comment":"Jo szarast3"},{"name":"Terem3","distance":"1.00","direction":"130","kind":"Terem","comment":"Jo tanulast3"}],"neighbourBeacons":{"1,2":{"distance":"15.00","direction":"123.23","comment":"Comment a 1,2es beaconhoz vezeto utra"},"1,4":{"distance":"9.00","direction":"123.23","comment":"Comment a 1,4es beaconhoz vezeto utra"}}},{"major":"1","minor":"4","idName":"1,4","triggerDistance":"0.70","pois":[{"name":"Retyo4","distance":"7.00","direction":"120","kind":"WC","comment":"Jo szarast4"},{"name":"Terem4","distance":"10.00","direction":"130","kind":"Terem","comment":"Jo tanulast4"}],"neighbourBeacons":{"1,3":{"distance":"9.00","direction":"123.23","comment":"Comment a 1,3es beaconhoz vezeto utra"}}}]
